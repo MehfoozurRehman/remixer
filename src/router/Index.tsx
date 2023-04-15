f6e82ee0d50a5d67c8f71c1d70fee78e457f1c16
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-04-15 23:05:21 +0500

    converted SuspenseAfterInitialRender to a differetn npm package

--- src/router/Index.tsx
+++ src/router/Index.tsx
@@ -4,4 +4,3 @@ export { default as Link } from "./Link";
 export { default as NavLink } from "./NavLink";
 export { default as Outlet } from "./Outlet";
 export { default as Router } from "./Router";
-export { default as SuspenseAfterInitialRender } from "./SuspenseAfterInitialRender";

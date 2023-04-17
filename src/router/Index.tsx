6bd2285d020e601519b7dcdd1c85bdde79e26fc6
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-04-17 15:06:22 +0500

    fix

--- src/router/Index.tsx
+++ src/router/Index.tsx
@@ -1,5 +1,4 @@
 export { default as Head } from "./Head";
 export { default as Link } from "./Link";
 export { default as NavLink } from "./NavLink";
-export { default as Outlet } from "./Outlet";
 export { default as Router } from "./Router";

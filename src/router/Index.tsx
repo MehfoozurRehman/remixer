870c0f116c305f87104e40970cd10e34b7fbce66
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-04-24 02:31:26 +0500

    removed custom outlet

--- src/router/Index.tsx
+++ src/router/Index.tsx
@@ -2,5 +2,4 @@ export { default as Head } from "./Head";
 export { default as Img } from "./Img";
 export { default as Link } from "./Link";
 export { default as NavLink } from "./NavLink";
-export { default as Outlet } from "./Outlet";
 export { default as Router } from "./Router";

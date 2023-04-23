6104e0432a9839a2ccfbaa0597884b02b865c240
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-04-23 19:24:13 +0500

    Revert "fix"
    This reverts commit 6bd2285d020e601519b7dcdd1c85bdde79e26fc6.

--- src/router/Index.tsx
+++ src/router/Index.tsx
@@ -2,4 +2,5 @@ export { default as Head } from "./Head";
 export { default as Img } from "./Img";
 export { default as Link } from "./Link";
 export { default as NavLink } from "./NavLink";
+export { default as Outlet } from "./Outlet";
 export { default as Router } from "./Router";

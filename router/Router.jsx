983b62c959a32ba8a117bb0e3350201b19092338
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-06-29 02:40:16 +0500

    Update Router.jsx

--- router/Router.jsx
+++ router/Router.jsx
@@ -46,88 +46,71 @@ const ErrorBoundaryLoad = async (routes, ...args) => {
   return Error ? Error(...args) : null;
 };
 
-function SegmentsGenerator(key) {
-  return key
-    .replace(/\/src\/screens|\.jsx|\[\.{3}.+\]|\.lazy/g, "")
-    .replace(/\[(.+)\]/g, ":$1")
-    .toLowerCase()
-    .split("/")
-    .filter((p) => !p.includes("_") && p !== "");
-}
-
-function RouteGenerator(lazys, key, eagers) {
-  const module = eagers === null ? lazys[key] : eagers[key];
-  const Component = eagers ? module.default : lazy(module);
-  const preload = eagers ? null : module;
-
-  const loader = eagers ? eagers?.[key]?.loader : Loader(module);
-  const action = eagers ? eagers?.[key]?.action : Action(module);
-  const ErrorBoundary = eagers
-    ? eagers?.[key]?.Error
-    : ErrorBoundaryLoad(module);
-
-  return {
-    Component,
-    loader,
-    action,
-    ErrorBoundary,
-    preload,
-  };
-}
-
-function RoutePositionGenerator(segments, routes, route) {
-  const insertRoute = (parent, segment, index) => {
-    const path = segment.replace(/index|\./g, "");
-    const root = index === 0;
-    const leaf = index === segments.length - 1 && segments.length > 1;
-    const node = !root && !leaf;
-    const insert = /^\w|\//.test(path) ? "unshift" : "push";
-
-    if (root) {
-      const dynamic = path.startsWith("[") || path === "*";
-      if (dynamic) return parent;
-      const last = segments.length === 1;
-      if (last) {
-        routes.push({ path, ...route });
-        return parent;
-      }
-    }
-
-    if (root || node) {
-      const current = root ? routes : parent.children;
-      const found = current?.find((route) => route.path === path);
-      found
-        ? (found.children ??= [])
-        : current?.[insert]({ path: path, children: [] });
-      return found || current?.[insert === "unshift" ? 0 : current.length - 1];
-    }
-
-    if (leaf) {
-      parent?.children?.[insert]({ path: path.replace(/\/$/, ""), ...route });
-    }
-
-    return parent;
-  };
-
-  segments.reduce(insertRoute, {});
-
-  return routes;
-}
-
 function RoutesReducer(eagers, lazys) {
   return Object.keys(eagers ?? lazys).reduce((routes, key) => {
-    const { Component, loader, action, ErrorBoundary, preload } =
-      RouteGenerator(lazys, key, eagers);
+    const module = eagers === null ? lazys[key] : eagers[key];
+    const Component = eagers ? module.default : lazy(module);
+    const preload = eagers ? null : module;
 
-    const segments = SegmentsGenerator(key);
+    const loader = eagers ? eagers?.[key]?.loader : Loader(module);
+    const action = eagers ? eagers?.[key]?.action : Action(module);
+    const ErrorBoundary = eagers
+      ? eagers?.[key]?.Error
+      : ErrorBoundaryLoad(module);
 
-    return RoutePositionGenerator(segments, routes, {
+    const segments = key
+      .replace(/\/src\/screens|\.jsx|\[\.{3}.+\]|\.lazy/g, "")
+      .replace(/\[(.+)\]/g, ":$1")
+      .toLowerCase()
+      .split("/")
+      .filter((p) => !p.includes("_") && p !== "");
+
+    const route = {
       Component,
       loader,
       action,
       ErrorBoundary,
       preload,
-    });
+    };
+
+    const insertRoute = (parent, segment, index) => {
+      const path = segment.replace(/index|\./g, "");
+      const root = index === 0;
+      const leaf = index === segments.length - 1 && segments.length > 1;
+      const node = !root && !leaf;
+      const insert = /^\w|\//.test(path) ? "unshift" : "push";
+
+      if (root) {
+        const dynamic = path.startsWith("[") || path === "*";
+        if (dynamic) return parent;
+        const last = segments.length === 1;
+        if (last) {
+          routes.push({ path, ...route });
+          return parent;
+        }
+      }
+
+      if (root || node) {
+        const current = root ? routes : parent.children;
+        const found = current?.find((route) => route.path === path);
+        found
+          ? (found.children ??= [])
+          : current?.[insert]({ path: path, children: [] });
+        return (
+          found || current?.[insert === "unshift" ? 0 : current.length - 1]
+        );
+      }
+
+      if (leaf) {
+        parent?.children?.[insert]({ path: path.replace(/\/$/, ""), ...route });
+      }
+
+      return parent;
+    };
+
+    segments.reduce(insertRoute, {});
+
+    return routes;
   }, []);
 }
 

818e1593298ceeb6960dccf31b94a1f7b96c4097
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-06-29 02:10:20 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,21 +9,12 @@ import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   resolve: {
-    alias: [
-      {
-        find: "@components",
-        replacement: "/src/components",
-      },
-      {
-        find: "@assets",
-        replacement: "/src/assets",
-      },
-      {
-        find: "@router",
-        replacement: "/router",
-      },
+    alias: {
+      "@components": "/src/components",
+      "@assets": "/src/assets",
+      "@router": "/router",
       ...config.alias,
-    ],
+    },
   },
   plugins: [
     config.compression && chunkSplitPlugin(),
@@ -36,45 +27,20 @@ export default defineConfig({
     config.progressiveWebApp && VitePWA({ registerType: "autoUpdate" }),
     config.imagesOptimization &&
       viteImagemin({
-        gifsicle: {
-          optimizationLevel: 7,
-          interlaced: false,
-        },
-        optipng: {
-          optimizationLevel: 7,
-        },
-        mozjpeg: {
-          quality: 30,
-        },
-        pngquant: {
-          quality: [0.7, 0.8],
-          speed: 4,
-        },
-        webp: {
-          quality: 70,
-        },
+        gifsicle: { optimizationLevel: 7, interlaced: false },
+        optipng: { optimizationLevel: 7 },
+        mozjpeg: { quality: 30 },
+        pngquant: { quality: [0.7, 0.8], speed: 4 },
+        webp: { quality: 70 },
         svgo: {
           multipass: true,
           plugins: [
-            {
-              name: "removeViewBox",
-            },
-            {
-              name: "minifyStyles",
-            },
-            {
-              name: "removeMetadata",
-            },
-            {
-              name: "removeUselessStrokeAndFill",
-            },
-            {
-              name: "reusePaths",
-            },
-            {
-              name: "removeEmptyAttrs",
-              active: true,
-            },
+            { name: "removeViewBox" },
+            { name: "minifyStyles" },
+            { name: "removeMetadata" },
+            { name: "removeUselessStrokeAndFill" },
+            { name: "reusePaths" },
+            { name: "removeEmptyAttrs", active: true },
           ],
         },
       }),

5fb86ab66d17c2b9cf6747f7b96fc0ff67ba3fee
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-06-30 01:44:17 +0500

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,30 +1,38 @@
-import * as fs from 'fs';
+import fs from 'fs';
+import path from 'path';
+
 const CURR_DIR = process.cwd();
+const NPM_IGNORE_FILE = '.npmignore';
+const GIT_IGNORE_FILE = '.gitignore';
 
-const generator = (templatePath, newProjectPath) => {
-  const filesToCreate = fs.readdirSync(templatePath);
+const generator = async (templatePath, newProjectPath) => {
+  try {
+    const filesToCreate = await fs.promises.readdir(templatePath);
 
-  filesToCreate.forEach(file => {
-    const origFilePath = `${templatePath}/${file}`;
+    for (const file of filesToCreate) {
+      const origFilePath = path.join(templatePath, file);
+      const stats = await fs.promises.stat(origFilePath);
 
-    // get stats about the current file
-    const stats = fs.statSync(origFilePath);
+      if (stats.isFile()) {
+        let targetFile = file;
 
-    if (stats.isFile()) {
-      const contents = fs.readFileSync(origFilePath, 'utf8');
+        if (file === NPM_IGNORE_FILE) {
+          targetFile = GIT_IGNORE_FILE;
+        }
 
-      // Rename
-      if (file === '.npmignore') file = '.gitignore';
+        const writePath = path.join(CURR_DIR, newProjectPath, targetFile);
+        const contents = await fs.promises.readFile(origFilePath, 'utf8');
+        await fs.promises.writeFile(writePath, contents, 'utf8');
+      } else if (stats.isDirectory()) {
+        const newDirPath = path.join(CURR_DIR, newProjectPath, file);
+        await fs.promises.mkdir(newDirPath);
 
-      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
-      fs.writeFileSync(writePath, contents, 'utf8');
-    } else if (stats.isDirectory()) {
-      fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
-
-      // recursive call
-      generator(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+        await generator(path.join(templatePath, file), path.join(newProjectPath, file));
+      }
     }
-  });
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 };
 
 export default generator;

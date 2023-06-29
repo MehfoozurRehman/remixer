c9f790c8d879c218712dfc82517ec1309d996247
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-06-30 02:41:02 +0500

    opitional deps install

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 
 import { dirname } from 'path';
+import { execSync } from 'child_process';
 import { fileURLToPath } from 'url';
 import generator from './generator.js';
 import inquirer from 'inquirer';
@@ -29,6 +30,12 @@ const QUESTIONS = [
       if (CHOICES.includes(input)) return true;
       else return 'Please select a valid project template.';
     },
+  },
+  {
+    name: 'install-deps',
+    type: 'confirm',
+    message: 'Do you want to install dependencies?',
+    default: true,
   },
 ];
 
@@ -61,6 +68,25 @@ const createProject = async () => {
     console.log(`Creating project '${projectName}' from template '${projectChoice}'...`);
     generator(templatePath, projectName, projectName);
     console.log('Project generation completed.');
+
+    const installDeps = answers['install-deps'];
+    if (installDeps) {
+      const packageManagerAnswer = await inquirer.prompt([
+        {
+          name: 'package-manager',
+          type: 'list',
+          message: 'Select a package manager:',
+          choices: ['npm', 'yarn'],
+        },
+      ]);
+      const packageManager = packageManagerAnswer['package-manager'];
+      console.log(`Installing dependencies with ${packageManager}...`);
+      const installCommand = packageManager === 'yarn' ? 'yarn install' : 'npm install';
+      execSync(installCommand, { cwd: projectPath, stdio: 'inherit' });
+      console.log('Dependencies installed successfully.');
+    }
+
+    console.log('Project setup completed.');
   } catch (error) {
     console.error('An error occurred:', error);
   }

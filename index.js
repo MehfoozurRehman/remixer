86c3c40a3f7c635afc665b103974e7935da65270
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-06-30 01:35:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const QUESTIONS = [
     message: 'Project name:',
     validate: function (input) {
       if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
-      else return 'Project name may only include letters, numbers, underscores and hashes.';
+      else return 'Project name may only include letters, numbers, underscores, and hashes.';
     },
   },
   {
@@ -27,15 +27,50 @@ const QUESTIONS = [
     type: 'list',
     message: 'What project template would you like to generate?',
     choices: CHOICES,
+    validate: function (input) {
+      if (CHOICES.includes(input)) return true;
+      else return 'Please select a valid project template.';
+    },
   },
 ];
 
-inquirer.prompt(QUESTIONS).then(answers => {
-  const projectChoice = answers['project-choice'];
-  const projectName = answers['project-name'];
-  const templatePath = `${__dirname}/templates/${projectChoice}`;
+const createProject = async () => {
+  try {
+    const answers = await inquirer.prompt(QUESTIONS);
+    const projectChoice = answers['project-choice'];
+    const projectName = answers['project-name'];
+    const templatePath = `${__dirname}/templates/${projectChoice}`;
+    const projectPath = `${CURR_DIR}/${projectName}`;
 
-  fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+    if (fs.existsSync(projectPath)) {
+      const overwriteAnswer = await inquirer.prompt([
+        {
+          name: 'overwrite',
+          type: 'confirm',
+          message: `A directory named '${projectName}' already exists. Do you want to overwrite it?`,
+          default: false,
+        },
+      ]);
+      if (!overwriteAnswer['overwrite']) {
+        console.log('Aborted. Please choose a different project name.');
+        return;
+      }
+    }
 
-  generator(templatePath, projectName);
-});
+    fs.mkdirSync(projectPath);
+    console.log(`Creating project '${projectName}' from template '${projectChoice}'...`);
+    generator(templatePath, projectName);
+    console.log('Project generation completed.');
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+};
+
+const handleCancellation = () => {
+  console.log('\nUser cancelled the prompt.');
+  process.exit();
+};
+
+process.on('SIGINT', handleCancellation);
+
+createProject();
